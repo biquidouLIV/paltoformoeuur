3abdf7819b45e98a4d51c847301c4f066b42c550
Hippolyte <jombarthippolyte@gmail.com>
2026-03-22 09:16:40 +0100

    champignon bumper (une base)

---
+++ paltoformoeuur/Assets/Scripts/Bumper.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    [SerializeField] private float strength;
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        other.rigidbody.AddForce(Vector3.up * strength);
+    }
+}

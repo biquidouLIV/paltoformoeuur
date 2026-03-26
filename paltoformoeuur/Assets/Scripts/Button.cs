6c185b7c5c2248e639d7bb42061b05e561e6fbfb
Liam <ltconflans@gmail.com>
2026-03-26 14:35:02 +0100

    bouton + porte

---
+++ paltoformoeuur/Assets/Scripts/Button.cs
@@ -0,0 +1,22 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] private GameObject door;
+    [SerializeField] private Vector3 doorMove = new Vector3(0,3,0);
+    [SerializeField] private float doorOpeningTime;
+
+    private bool isOpen;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(isOpen) return;
+        
+        Debug.Log("button pressed");
+        door.transform.DOMove(door.transform.position + doorMove, doorOpeningTime)
+            .SetEase(Ease.InOutCubic);
+        isOpen = true;
+
+    }
+}

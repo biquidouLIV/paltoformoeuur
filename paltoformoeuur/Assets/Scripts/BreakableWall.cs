89a1b0fb36afa797fdbd72e1cd980e93005940c1
Hippolyte <jombarthippolyte@gmail.com>
2026-05-18 14:02:23 +0200

    garder velocite tete

--- paltoformoeuur/Assets/Scripts/BreakableWall.cs
+++ paltoformoeuur/Assets/Scripts/BreakableWall.cs
@@ -1,14 +1,16 @@
+using System;
 using UnityEngine;
 
 public class BreakableWall : MonoBehaviour
 {
     [SerializeField] private float velocityToBreak;
-
-    private void OnCollisionEnter2D(Collision2D other)
+    
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Head"))
         {
-            if (other.relativeVelocity.magnitude > velocityToBreak)
+            Rigidbody2D rigidbodyD = other.gameObject.GetComponent<Rigidbody2D>();
+            if (rigidbodyD.linearVelocity.magnitude > velocityToBreak)
             {
                 Destroy(gameObject);
             }

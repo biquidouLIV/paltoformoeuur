d47ef3576288dbee2855f0c41c421469abd35e5a
Liam <ltconflans@gmail.com>
2026-05-27 10:49:57 +0200

    menu poze ki marsh
    on peut pas encore utiliser la manette sur l'UI
    j'ai mis UIManager sur le canvas

--- paltoformoeuur/Assets/Scripts/UIManager.cs
+++ paltoformoeuur/Assets/Scripts/UIManager.cs
@@ -1,24 +1,35 @@
+using System;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public static UIManager instance;
+    
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject pauseMenu;
     private float actualTimeScale;
 
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(this);
+    }
+
     public void Play()
     {
+        if(mainMenu == null) return;
         mainMenu.SetActive(false);
     }
     
     public void Quit()
     {
+        if(mainMenu == null) return;
         mainMenu.SetActive(false);
     }
 
     public void Pause()
     {
-        
+        if(pauseMenu == null) return;
         pauseMenu.SetActive(!pauseMenu.activeSelf);
         if (pauseMenu.activeSelf)
         {

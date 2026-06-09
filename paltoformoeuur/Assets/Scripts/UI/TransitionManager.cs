3b7d03e9c249b083aea9f65dfcc37ebf89d4c053
Hippolyte <jombarthippolyte@gmail.com>
2026-06-09 18:55:21 +0200

    Jeu qui marche (revenir à cette version si on a pas de build à 00h)
    ps comment ca il est 19h je pensais qu'il était 17h

---
+++ paltoformoeuur/Assets/Scripts/UI/TransitionManager.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionManager : MonoBehaviour
+{
+    public static TransitionManager instance;
+    
+    [Header("transition")]
+    [SerializeField] private RectTransform transitionScreen;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(this);
+    }
+
+    private void Start()
+    {
+        transitionScreen.gameObject.SetActive(true);
+    }
+
+    public IEnumerator TransitionOpen()
+    {
+        yield return new WaitForSeconds(0.5f);
+        transitionScreen.localPosition = new Vector3(0, 0, 0);
+        transitionScreen.DOLocalMove(new Vector3(-1920, 0, 0), 1).SetUpdate(true);
+    }
+    
+    public void LoadScene(int scene)
+    {
+        transitionScreen.localPosition = new Vector3(1920, 0, 0);
+        transitionScreen.DOLocalMove(new Vector3(0, 0, 0), 1)
+            .SetUpdate(true)
+            .OnComplete((() =>
+            {
+                SceneManager.LoadScene(scene);
+            }));
+    }
+
+}
